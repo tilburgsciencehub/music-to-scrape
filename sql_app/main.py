f2a878dd5d4fd71cf9fa8aeeb7f337b48817f62b
thierrylahaye <65568251+thierrylahaije@users.noreply.github.com>
2023-03-20 15:48:25 +0100

    Update main.py
    The main.py file was not added when update the documentation from Swagger to Redoc. The Redoc documentation is now complete and the other unnecessary functions have been removed or made invisible

--- sql_app/main.py
+++ sql_app/main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import api
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import HTMLResponse
+from fastapi.openapi.utils import get_openapi
 
 # Initialize the app
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url="/docs")
 
 app.include_router(api.router)
