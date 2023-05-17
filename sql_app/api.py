f8b6b94ec1b58e9cd841d8efdfaa76cea462f25b
Hannes Datta <h.datta@tilburguniversity.edu>
2023-05-17 11:17:05 +0200

    rename endpoints and add documentation
    Former-commit-id: b2b5c9ec7c18951df5a9993f863e606b8d80d2b2 [formerly b2b5c9ec7c18951df5a9993f863e606b8d80d2b2 [formerly d07c9972a9a03048edf04a135184e9cafbd68221]]
    Former-commit-id: 475f6a2be5b0a4cefd588401bab7c63da57df198
    Former-commit-id: d2f35f2e300713148f32765675c69a43eea67b86

--- sql_app/api.py
+++ sql_app/api.py
@@ -9,15 +9,6 @@ from schemas import User, CreateAndUpdateCar, PaginatedUserInfo, CountPlays, Use
 
 router = APIRouter()
     
-# API to get the list of user info
-@router.get("/users", response_model=PaginatedUserInfo)
-def list_users(limit: int = 10, offset: int = 0, session: Session = Depends(get_db)):
-
-    cars_list = get_all_users(session, limit, offset)
-    response = {"limit": limit, "offset": offset, "data": cars_list}
-
-    return response
-
 #user.getInfo
 @router.get("/user/getinfo", response_model=UserFindClass)
 def get_user_info(username: str, session: Session = Depends(get_db)):
@@ -29,7 +20,7 @@ def get_user_info(username: str, session: Session = Depends(get_db)):
         raise HTTPException(**cie.__dict__)
 
 #user.getTotalPlays
-@router.get("/totalplays", response_model=CountPlays)
+@router.get("/user/plays", response_model=CountPlays)
 def get_total_plays_for_username(username: str, session: Session = Depends(get_db)):
 
     try:
@@ -39,7 +30,7 @@ def get_total_plays_for_username(username: str, session: Session = Depends(get_d
         raise HTTPException(**cie.__dict__)
     
 #user.getRecentTracks    
-@router.get("/user-recent-tracks", response_model= RecentTracks)
+@router.get("/user/recent-tracks", response_model= RecentTracks)
 def get_recent_tracks(username: str, limit: int = 5, start: int = 0 , end: int = 2000000000, session: Session = Depends(get_db)):
 
     try:
@@ -50,7 +41,7 @@ def get_recent_tracks(username: str, limit: int = 5, start: int = 0 , end: int =
         raise HTTPException(**cie.__dict__)
 
 #user.getTopArtists
-@router.get("/user-top-artists", response_model= TopArtistsUser)
+@router.get("/user/top-artists", response_model= TopArtistsUser)
 def get_user_top_artists(username: str, limit: int = 5, session: Session = Depends(get_db), period: str = "overall"):
 
     try:
@@ -61,7 +52,7 @@ def get_user_top_artists(username: str, limit: int = 5, session: Session = Depen
         raise HTTPException(**cie.__dict__)
     
 #user.getTopTracks
-@router.get("/user-top-tracks", response_model= TopTracksUser)
+@router.get("/user/top-tracks", response_model= TopTracksUser)
 def get_user_top_tracks(username: str, limit: int = 5, session: Session = Depends(get_db), period: str = "overall"):
 
     try:
@@ -72,7 +63,7 @@ def get_user_top_tracks(username: str, limit: int = 5, session: Session = Depend
         raise HTTPException(**cie.__dict__)
 
 #user.getWeeklyTrackChart
-@router.get("/weekly-track-chart-user", response_model = WeeklyTrackUserChart)
+@router.get("/user/weekly-track-chart", response_model = WeeklyTrackUserChart)
 def get_weekly_track_chart_user(username: str, session: Session = Depends(get_db)):
 
     try:
@@ -81,9 +72,10 @@ def get_weekly_track_chart_user(username: str, session: Session = Depends(get_db
     
     except CarInfoException as cie:
         raise HTTPException(**cie.__dict__)
-    
+
+
 #user.getWeeklyArtistChart
-@router.get("/weekly-artist-chart-user", response_model = WeeklyArtistUserChart)
+@router.get("/user/weekly-artist-chart", response_model = WeeklyArtistUserChart)
 def get_weekly_artist_chart_user(username: str, session: Session = Depends(get_db), limit: int = 5):
 
     try:
@@ -93,50 +85,19 @@ def get_weekly_artist_chart_user(username: str, session: Session = Depends(get_d
     except CarInfoException as cie:
         raise HTTPException(**cie.__dict__)
     
-#artist.getTopTracks
-@router.get("/artist-top-tracks", response_model= TopTracksArtist)
-def get_artist_top_tracks(artist: str, limit: int = 5, session: Session = Depends(get_db)):
 
-    try:
-        top_tracks = get_top_tracks_for_artist(session, artist, limit)
+# API to get the list of user info
+@router.get("/users", response_model=PaginatedUserInfo)
+def list_users(limit: int = 10, offset: int = 0, session: Session = Depends(get_db)):
 
-        if "_" in artist:
-            artist_name = artist.replace("_", " ")
-    
-        else:
-            artist_name = artist
-    
-        return {"artist": artist_name, "top_tracks": top_tracks}
-    
-    except CarInfoException as cie:
-        raise HTTPException(**cie.__dict__)
-    
-#chart.getTopTracks
-@router.get("/chart-top-tracks", response_model= ChartTopTracks)
-def chart_get_top_tracks(week: int, limit: int = 5, session: Session = Depends(get_db), year: int = 2023):
+    cars_list = get_all_users(session, limit, offset)
+    response = {"limit": limit, "offset": offset, "data": cars_list}
 
-    try:
-        top_tracks = get_chart_top_tracks(session, limit, year, week)
-    
-        return {"week": week, "chart": top_tracks}
-    
-    except CarInfoException as cie:
-        raise HTTPException(**cie.__dict__)
-    
-#chart.getTopArtists
-@router.get("/chart-top-artists", response_model= ChartTopArtists)
-def chart_get_top_artists(week: int, limit: int = 5, session: Session = Depends(get_db), year: int = 2023):
+    return response
 
-    try:
-        top_artists = get_chart_top_artists(session, limit, year, week)
-    
-        return {"week": week, "chart": top_artists}
-    
-    except CarInfoException as cie:
-        raise HTTPException(**cie.__dict__)
     
 #user.getRecentlyActive    
-@router.get("/recently-active-users", response_model = RecentActiveUsers)
+@router.get("/users/recently-active", response_model = RecentActiveUsers)
 def get_recently_active_users(limit: int = 5, session: Session = Depends(get_db)):
 
     """
@@ -169,8 +130,37 @@ def get_recently_active_users(limit: int = 5, session: Session = Depends(get_db)
     except CarInfoException as cie:
         raise HTTPException(**cie.__dict__)
 
+#artist.getTopTracks
+@router.get("/artist/top-tracks", response_model= TopTracksArtist)
+def get_artist_top_tracks(artist: str, limit: int = 5, session: Session = Depends(get_db)):
+
+    try:
+        top_tracks = get_top_tracks_for_artist(session, artist, limit)
+
+        if "_" in artist:
+            artist_name = artist.replace("_", " ")
+    
+        else:
+            artist_name = artist
+    
+        return {"artist": artist_name, "top_tracks": top_tracks}
+    
+    except CarInfoException as cie:
+        raise HTTPException(**cie.__dict__)
+    
+#artist.getInfo
+@router.get("/artist/info", response_model = ArtistInfo)
+def artist_info(artist: str, session: Session = Depends(get_db)):
+
+    try:
+        artists = get_artist_info(session, artist)
+        return artists
+
+    except CarInfoException as cie:
+        raise HTTPException(**cie.__dict__)
+    
 #artist.featured
-@router.get("/featured-artists", response_model = ArtistFeaturedList)
+@router.get("/artists/featured", response_model = ArtistFeaturedList)
 def featured_artist(limit: int = 5, session: Session = Depends(get_db)):
 
     try:
@@ -180,13 +170,27 @@ def featured_artist(limit: int = 5, session: Session = Depends(get_db)):
     except CarInfoException as cie:
         raise HTTPException(**cie.__dict__)
 
-#artist.getInfo
-@router.get("/artist-info", response_model = ArtistInfo)
-def artist_info(artist: str, session: Session = Depends(get_db)):
+
+#chart.getTopTracks
+@router.get("/charts/top-tracks", response_model= ChartTopTracks)
+def chart_get_top_tracks(week: int, limit: int = 5, session: Session = Depends(get_db), year: int = 2023):
 
     try:
-        artists = get_artist_info(session, artist)
-        return artists
+        top_tracks = get_chart_top_tracks(session, limit, year, week)
+    
+        return {"week": week, "chart": top_tracks}
+    
+    except CarInfoException as cie:
+        raise HTTPException(**cie.__dict__)
+    
+#chart.getTopArtists
+@router.get("/charts/top-artists", response_model= ChartTopArtists)
+def chart_get_top_artists(week: int, limit: int = 5, session: Session = Depends(get_db), year: int = 2023):
 
+    try:
+        top_artists = get_chart_top_artists(session, limit, year, week)
+    
+        return {"week": week, "chart": top_artists}
+    
     except CarInfoException as cie:
         raise HTTPException(**cie.__dict__)
