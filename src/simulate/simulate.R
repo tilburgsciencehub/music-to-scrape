608db11f2e4364d560f372e6bf9f096e5d51336d
Hannes Datta <h.datta@tilburguniversity.edu>
2023-08-22 13:46:55 +0200

    speed up simulation

--- src/simulate/simulate.R
+++ src/simulate/simulate.R
@@ -2,6 +2,7 @@
 library(data.table)
 library(DBI)
 library(RSQLite)
+library(tidyverse)
 
 set.seed(1234)
 warning(getwd())
@@ -10,15 +11,18 @@ N_users = 500
 # load artist and song names
 raw <- fread("https://raw.githubusercontent.com/mahkaila/songnames/master/SongCSV.csv", quote="")
 
-for (col in colnames(raw)[which(unlist(lapply(raw, class)=='character'))]) raw[, (col):=gsub("b['|\"]","", get(col))]
-for (col in colnames(raw)[which(unlist(lapply(raw, class)=='character'))]) raw[, (col):=gsub("['|\"]$","", get(col))]
+# clean-up byte-look-alike strings
+selected_cols = colnames(raw)[which(unlist(lapply(raw, class)=='character'))]
+for (col in selected_cols) raw[, (col):=gsub("b['|\"]","", get(col))]
+for (col in selected_cols) raw[, (col):=gsub("['|\"]$","", get(col))]
+for (col in selected_cols) raw[, (col):=gsub('^["]',"", get(col))]
+for (col in selected_cols) raw[, (col):=gsub("['|\"]$","", get(col))]
+
 raw[Year==0, Year:=NA]
 
 
-#artists <- fread('../../data/apis-to-scrape-artists.csv')
 artists = unique(raw, by = c('ArtistID'))[, c('ArtistID','ArtistName','ArtistLocation'),with=F]
 artists[, sampled:=runif(.N)]
-#artist_names = artists[sampled<=.05]$ArtistName
 artists[, popularity:=runif(.N)]
 
 songs = unique(raw, by = c('SongID'))[, c('ArtistID','ArtistName','SongID','AlbumID','Danceability','Duration','KeySignature','Tempo','TimeSignature','Title', 'Year')]
@@ -27,35 +31,41 @@ setkey(songs, ArtistID)
 setkey(artists, ArtistID)
 songs[artists, artist_popularity:=i.popularity]
 
-# artist and song names
-dates = seq(from=as.Date('2023-01-01'), to = as.Date('2023-06-01'), by = '1 day') #12-31'), by = '1 day')
+# set time horizon
+dates = seq(from=as.Date('2023-07-01'), to = as.Date('2024-06-01'), by = '1 day')
 
 usernames = paste0('user', 1:N_users)
 user_active = runif(length(usernames)) # probability that a user, on any given day listens
 usage_intensity = rpois(length(usernames), 12) # approximate number of songs consumed
 
 # simulate data
-# song_lengths = seq(from=180, to=240, by = 1)
 
 history = rbindlist(lapply(usernames, function(user) {
-  rbindlist(lapply(dates, function(date) {
-    # play music?
-    play = runif(1)<=user_active[which(user==usernames)]
-    if (play==FALSE) return(list())
-    
-    # which artists get consumed?
-    nsongs = max(1,rpois(1,usage_intensity[which(user==usernames)]))
-    selected_songs=songs[match(sample(songs$SongID, nsongs, prob=songs$artist_popularity),SongID)]
-    
-    # listening offset: when does the session start?
-    offset = as.numeric(sample(seq(as.POSIXct(date), as.POSIXct(date+1), by="1 mins"),1))
-    
-    unix = cumsum(c(offset, rev(rev(selected_songs$Duration)[-1])))
-      
-    usage = data.table(user = user, date = date, timestamp = unix, artist_id=selected_songs$ArtistID, song_id = selected_songs$SongID, unique_id = paste0(unix,selected_songs$ArtistID,selected_songs$SongID))
-    return(usage)
+  play = data.table(date=dates, active=runif(length(dates))<=user_active[which(user==usernames)])
+  play[, nsongs:=pmax(1,rpois(.N,usage_intensity[which(user==usernames)]))]
+  play[, date_unix:=as.numeric(as.POSIXct(date))]
+  play[active==F, nsongs:=0]
+  play[active==T, offset:=sample(0:(3600*24-1),.N)]
+  play[active==T, timestamp_session_start:=date_unix+offset]
+  # generate play timestamps
+  
+  timestamps = rep(play[active==T]$timestamp_session_start, play[active==T]$nsongs)
+  my_dates = rep(play[active==T]$date, play[active==T]$nsongs)
+  
+  selected_songs=songs[match(sample(songs$SongID, sum(play[active==T]$nsongs), prob=songs$artist_popularity),SongID)]
+  selected_songs[, date:=my_dates]
+  selected_songs[, session_start_unix:=timestamps]
+  
+  selected_songs[, unix:=cumsum(c(session_start_unix[1], rev(rev(Duration)[-1]))), by = c('date')]
+                
+        
+  usage = selected_songs[, list(user = user, date = date, timestamp = unix, 
+                         artist_id=ArtistID, song_id = SongID, unique_id = paste0(unix,ArtistID,SongID))]
+  
+  return(usage)
   }))
-}))
+
+history <- history[!is.na(date)]
 
 # create user demo
 set.seed(1234)
